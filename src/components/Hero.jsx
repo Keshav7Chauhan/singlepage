05c269cdcd1b0f1a5902546e031ba191d2be1970
Your Name <your.email@example.com>
2024-03-18 15:08:35 +0530

    change p tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,8 @@ const Hero=()=> {
     <main className='hero'>
       <div className='hero-content'></div>
       <div className='hero-image'></div>
+      <p>hello world</p>
+      <h1>I am a full stack developer.</h1>
     </main>
   )
 }
